a28e4dd3a5954acc49b5bac1bae7ecdb31eefad6
Lucas Pavanelli <lucasapava@gmail.com>
2014-10-04 09:32:17 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MonstersInc::Application.config.secret_key_base = '669dc560003b3905104eed5d60d11efc01e13a5401703a84c6b11d8a77fef812938ea71566162c0846df6d8dc559cb948427fd8a1c43cdcb90899cdea99ecd1e'
